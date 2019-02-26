9ded76e805bb1fd69fc6d6dda3ad9ded89f4082e
Sirokuma <sirokuma@irisu.cc>
2019-02-26 11:40:24 +0800

    2.26

--- asio_study/main.cpp
+++ asio_study/main.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+#include<boost/asio.hpp>
+using namespace boost;
 //basic
 //在创建端点（endpoint）之前，客户端应用程序必须获取原始IP地址和指定将与之通信的服务器的协议端口号
 //另一方面，服务器应用程序通常侦听所有IP地址上的传入消息，只需要获取要侦听的端口号
@@ -66,3 +69,194 @@ public:
 //2.创建表示传输层协议(TCP/UDP)的类的对象以及套接字要与之通信的基础IP协议(IPv4/IPv6)的版本
 //3.创建表示与所需协议类型对应的套接字的对象,将asio::io_service类的对象传递给套接字的构造函数
 //4.调用套接字的open()方法,将表示在步骤2中创建的协议的对象作为参数传递
+int Create_TCP_Socket_client_1() {
+	//step 1 
+	//实例化io_service,它是Boost.Asio的核心组件,提供对底层操作系统的网络I/O
+	//服务的访问,Boost.Asio套接字可以通过此类的对象访问这些服务
+	asio::io_service ios;
+
+	//step 2
+	//此类表示TCP协议,它不提供任何功能,而是像一个包含一组描述协议的值的数据结构
+	//asio::ip::tcp类没有公共构造函数,相反它提供两个静态方法
+	//v4(),v6(),它们返回asio::ip::tcp类的对象,表示TCP协议与底层IPv4或IPv6协议相对应
+	asio::ip::tcp protocol = asio::ip::tcp::v4();
+	//此外,这个类还包含一些旨在与TCP协议一起使用的基本类型的声明,
+	//其中包括asio::ip::tcp::endpoint asio::ip::tcp::socket asio::ip::tcp::acceptor等
+	//在boost/asio/ip/tcp.hpp中
+	//
+	/*namespace boost {
+	namespace asio {
+	namespace ip {
+	// ...*/
+	/* class tcp
+	{
+	public:
+	/// The type of a TCP endpoint.
+	typedef basic_endpoint<tcp> endpoint;
+
+	// ...
+
+	/// The TCP socket type.
+	typedef basic_stream_socket<tcp> socket;
+	/// The TCP acceptor type.
+	typedef basic_socket_acceptor<tcp> acceptor;
+
+	// ...*/
+
+	//step 3
+	//创建一个asio::ip::tcp::socket类的对象将io_service类的对象作为参数传递给它的构造函数
+	//注意:此构造函数不分配底层操作系统的套接字对象
+	//当调用open方法并将指定协议的对象作为参数传递给它时,在step4中分配实际操作系统的套接字
+	//在boost.asio中,打开(open)套接字意味着将它与描述套接字要通信的特定协议的完整参数集相关联
+	//当Boost.Asio套接字对象提供这些参数时,它有足够的信息来分配底层操作系统的真实套接字对象
+	//asio::ip::tcp::socket类提供了另一个接受协议对象作为参数的构造函数,此构造函数构造一个套接字并将其打开(open)
+	//注意:如果失败,此构造函数将抛出boost::system::system_error类型的异常
+	asio::ip::tcp::socket sock(ios);
+
+
+	//用于存储open socket的错误
+	boost::system::error_code ec;
+
+	//step 4
+	sock.open(protocol, ec);
+
+	if (ec.value() != 0)
+	{
+		//Failed
+		std::cout
+			<< "Failed, Error code = "
+			<< ec.value() << ", Message: " << ec.message();
+		return ec.value();
+	}
+	return 0;
+	//可以用以下方法替换3-4
+	try {
+		asio::ip::tcp::socket sock2(ios, protocol);
+
+	}
+	catch (boost::system::system_error &e) {
+		std::cout << "Error, Code = "
+			<< e.code() << ", Message: " << e.what();
+	}
+}
+//以下是创建一个UDP的例子
+int Create_UDP_Socket_Client_1() {
+	asio::io_service ios;
+
+	asio::ip::udp protocol = asio::ip::udp::v6();
+
+	boost::system::error_code ec;
+	asio::ip::udp::socket sock(ios);
+
+	sock.open(protocol, ec);
+
+	if (ec.value() != 0)
+	{
+		//Failed
+		std::cout
+			<< "Failed, Error code = "
+			<< ec.value() << ", Message: " << ec.message();
+		return ec.value();
+	}
+	return 0;
+}
+//创建被动套接字passive socket/acceptor
+//它是一种套接字,用于等待来自通过TCP协议进行通信的远程应用程序的连接建立请求
+//此定义有两个重要含义
+//1.被动套接字仅用于服务器应用程序或可能同时扮演客户端和服务器角色混合应用程序
+//2.仅为TCP协议定义被动套接字,由于UDP协议并不意味着建立连接,因此通过UDP执行通信时不需要被动套接字
+
+//在Boost.Asio中,被动套接字由asio::ip::tcp::acceptor类表示
+//类的名称建议类的对象的关键功能监听和接受或处理传入的连接请求
+//1.创建asio::io_service类的实例或使用之前创建的实例
+//2.创建asio::ip::tcp类的对象,该对象表示TCP协议和基础IP协议(IPv4/IPv6)的所需版本
+//3.创建表示接受器套接字的asio::ip::tcp::acceptor类的对象,将asio::io_service类的对象传递给其构造函数
+//4.调用acceptor socket的open()方法,将表示在2中创建的协议的对象作为参数传递
+
+int Create_TCP_Acceptor_Server() {
+	//step 1
+	//所有需要访问底层操作系统服务的Boost.Asio组件都需要此类
+	asio::io_service ios;
+
+	//step 2
+	//创建一个表示TCP协议的对象
+	asio::ip::tcp protocol = asio::ip::tcp::v4();
+
+	//step 3
+	//就像在活动套接字中一样,此构造函数实例化了Boost.Asio的对象asio::ip::tcp::acceptor类,介不分配底层操作系统的实际套接字对象
+	asio::ip::tcp::acceptor ac(ios);
+
+	boost::system::error_code ec;
+
+	//step 4
+	//扣件系统套接字对象在步骤4中分配,调用open()方法将协议对象作为参数传递给它
+	ac.open(protocol, ec);
+
+	//
+	if (ec.value() != 0)
+	{
+		//Failed
+		std::cout
+			<< "Failed, Error code = "
+			<< ec.value() << ", Message: " << ec.message();
+		return ec.value();
+	}
+	return 0;
+}
+
+//解析一个DNS名字
+//原始IP地址不便于人们感知和记忆
+//为了使用友好的名称票房网络中的设备,引入了域名系统(DNS)
+//DNS是一种分布式命名系统允许将人性化的名称与计算机网络中的设备相关联
+//确切地说,DNS名称是一个或多个IP地址的别名,但不是设备的别名
+//它没有命名特定的物理设备,而是可以分配给设备的IP地址
+//DNS充当分布式数据库,存储DNS名称到相应IP地址的映射到的IP地址,将DNS名称转换为相应IP地址的过程称为
+//DNS域名解析,现代网络操作系统包含可以查询DNS以解析DNS名称的功能,并提供可由应用程序用于执行DNS名称解析的接口
+
+//解析DNS名称以获得运行客户端应用程序要与之通信的服务器应用程序的主机(0个或多个)的IP地址(0 or 0+)所需的步骤
+//1.获取指定服务器应用程序的DNS名称和协议端口号,并将它们表示为字符串
+//2.创建asio::io_service类的实例或使用之前创建的实例
+//3.创建表示DNS名称腹板查询的解析程序::查询类对象
+//4.创建和合必要协议的DNS名称解析程序类的实例
+//5.调用解析器的resolve()方法,将步骤3中创建的查询对象作为参数传递给它
+//Resolve DNS
+int Resolving_DNS() {
+	//step 1
+	std::string host = "sirokuma.cc";
+	std::string port_num = "80";
+
+	//step 2
+	asio::io_service ios;
+
+	//step 3
+	asio::ip::tcp::resolver::query resolver_query(host, port_num, asio::ip::tcp::resolver::query::numeric_service);
+
+	//step 4
+	asio::ip::tcp::resolver resolver(ios);
+
+	//用于存储解析过程中的错误
+	boost::system::error_code ec;
+
+	//step 5
+	asio::ip::tcp::resolver::iterator it =
+		resolver.resolve(resolver_query, ec);
+	asio::ip::tcp::endpoint ep = *it;
+	std::cout << ep.address() << std::endl;
+	//处理错误
+	if (ec.value() != 0)
+	{
+		//Failed
+		std::cout
+			<< "Failed, Error code = "
+			<< ec.value() << ", Message: " << ec.message();
+		return ec.value();
+	}
+	return 0;
+
+}
+int main() {
+	if (Resolving_DNS() != 0) {
+		return 1;
+	}
+	std::system("pause");
+}
